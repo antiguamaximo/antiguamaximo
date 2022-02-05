f3c31361e4284284daad6afebc64485cec8a2023
Max <81933239+antiguamaximo@users.noreply.github.com>
2022-02-04 22:24:02 -0500

    Delete add_csv.py

--- add_csv.py
+++
@@ -1,7 +0,0 @@
-from csv import writer
-new_info = ['Rampage', '1.03', '450','5','230']
-
-with open('Information.csv','a') as file:
-    writer_obj = writer(file)
-    writer_obj.writerow(new_info)
-    file.close()
